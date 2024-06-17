a4bdd538342aa3b3d20979bd0c7aae4072bff91d
binaryrain97 <binaryrain97@gmail.com>
2024-06-17 22:52:41 +0900

    '세팅'

---
+++ src/main/java/com/example/chartist/ChartistApplication.java
@@ -0,0 +1,13 @@
+package com.example.chartist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartistApplication.class, args);
+	}
+
+}
