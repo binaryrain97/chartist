00fd1792e6ff2972fdaf8537e20e61fcd3ddd5c4
binaryrain97 <binaryrain97@gmail.com>
2024-06-27 08:32:35 +0900

    jar 파일생성

--- src/main/java/com/example/chartist/handler/ChatHandler.java
+++ src/main/java/com/example/chartist/handler/ChatHandler.java
@@ -1,6 +1,8 @@
 package com.example.chartist.handler;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -13,23 +15,30 @@ import java.util.Map;
 @Component
 @Log4j2
 public class ChatHandler extends TextWebSocketHandler {
-    private static final Map<WebSocketSession, String> map = new HashMap<>();
+    private static final Map<String, WebSocketSession> sessions = new HashMap<>();
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        for(WebSocketSession sess: map.keySet()) {
-            sess.sendMessage(message);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String nickname = (String)session.getAttributes().get("nickname");
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName(); // 현재 인증된 사용자의 이름 가져오기
+        }
+        for(WebSocketSession sess: sessions.values()) {
+            String messageToSend = nickname + ":" + payload;
+            sess.sendMessage(new TextMessage(messageToSend));
         }
     }
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         String nickname = "익명" + session.getId();
-        map.put(session, nickname);
+        session.getAttributes().put("nickname", nickname);
+        sessions.put(session.getId(), session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        map.remove(session);
+        sessions.remove(session.getId());
     }
 }
